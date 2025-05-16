6d43002292eae6a6059eff1eab3ab188ac553ce4
dimitriksky <70145560+dimitriksky@users.noreply.github.com>
2025-05-16 11:03:07 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dmitri Carabetchi on 16.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
